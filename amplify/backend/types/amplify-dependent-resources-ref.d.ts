808d98c289a9b7de6f0bf5356126568d320dbb35
personal <margain.roberto@gmail.com>
2023-05-08 12:15:59 -0600

    new start

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++
@@ -1,9 +0,0 @@
-export type AmplifyDependentResourcesAttributes = {
-  "api": {
-    "operifysite": {
-      "GraphQLAPIEndpointOutput": "string",
-      "GraphQLAPIIdOutput": "string",
-      "GraphQLAPIKeyOutput": "string"
-    }
-  }
-}
